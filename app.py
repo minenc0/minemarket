fceb16c421a7dd2d5b3be0c2c4014c02222c7956
minenc0 <yondewasa@gmail.com>
2025-07-30 03:56:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, jsonify
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -10,7 +9,8 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_name("market-1-467405-88c302d95d37.json", scope)
 client = gspread.authorize(creds)
 
-SHEET_NAME = "RempahProduk"  # Sesuaikan dengan nama spreadsheet kamu
+# Spreadsheet ID dari link Google Sheets kamu
+SPREADSHEET_ID = "1GRQKGZU6cBUcN99D-4bTdXUU2lus8hU_3Mn-AUlmATg"
 
 @app.route("/")
 def index():
@@ -18,9 +18,10 @@ def index():
 
 @app.route("/produk")
 def produk():
-    sheet = client.open(SHEET_NAME).sheet1
+    sheet = client.open_by_key(SPREADSHEET_ID).sheet1
     data = sheet.get_all_records()
     return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
