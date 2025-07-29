e1077e58fc57ea8ac34bfe99f523d0e895db7283
minenc0 <yondewasa@gmail.com>
2025-07-29 13:18:50 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
+
 from flask import Flask, render_template, jsonify
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
-# Setup kredensial service account
+# Koneksi ke Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("market-1-467405-88c302d95d37.json", scope)
 client = gspread.authorize(creds)
 
-# Ganti dengan nama atau ID spreadsheet kamu
-SHEET_NAME = "RempahProduk"
+SHEET_NAME = "RempahProduk"  # Sesuaikan dengan nama spreadsheet kamu
 
 @app.route("/")
 def index():
